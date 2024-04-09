ae151b81cf87f4979c4bc2daf7f06af967b0e80b
Aditya Ashish Yadav <adiashy1006@gmail.com>
2024-04-09 13:57:11 +0530

    Added code

---
+++ src/main/java/com/adi685/expenseTrackerMongoDb/model/Expense.java
@@ -0,0 +1,32 @@
+package com.adi685.expenseTrackerMongoDb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@Document("expense")
+public class Expense {
+
+  @Id
+  private String id;
+
+  @Field("name")
+  @Indexed(unique = true )
+  private String expenseName;
+
+  @Field("category")
+  private ExpenseCategory expenseCategory;
+
+  @Field("discription")
+  private String expenseDiscription;
+
+  @Field("amount")
+   private BigDecimal expenseAmount;
+}

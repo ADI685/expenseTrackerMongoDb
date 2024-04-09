7c47cbd1a1fbf53aca3c3e4922e25d6d04470099
Aditya Ashish Yadav <adiashy1006@gmail.com>
2024-04-09 10:40:28 +0530

    FIRST commit

---
+++ src/main/java/com/adi685/expenseTrackerMongoDb/ExpenseTrackerMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.adi685.expenseTrackerMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adi685/expenseTrackerMongoDb/ExpenseTrackerMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adi685.expenseTrackerMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
